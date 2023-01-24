f42e8f6fc8252614d075b485fc30948f9cfeccc0
Lucas Vallejos <lucasvallejos14@hotmail.com>
2023-01-24 20:33:48 -0300

    primer commit de este intento de proyecto

---
+++ ejercicio.js
@@ -0,0 +1,45 @@
+class ProductManager {
+
+    constructor () {
+       
+        this.id = 0
+        this.products = []
+        
+    }
+
+
+       
+
+    addProduct(titulo, descripcion, precio, imagen, stock) {
+
+        this.products.push({
+            titulo : titulo,
+            descripcion : descripcion,
+            precio : precio,
+            imagen : imagen,
+            stock : stock, 
+        } )
+
+        this.id ++;
+
+    }
+    
+    getProductsById(id) {
+        return this.products.find( products => products.id ===id)
+    }
+
+}
+    
+
+ let productos = new ProductManager();
+
+productos.addProduct("Coca-Cola", "Sabor Original", "$100", "https://jumboargentina.vtexassets.com/arquivos/ids/441468/Coca-Cola-25-L-3-17483.jpg?v=636528846231600000", 20)
+productos.addProduct("Sprite", "Clasica", "$100", "https://www.repartienda.com/wp-content/uploads/2020/05/IMG_8513.jpg", 20)
+productos.addProduct("Fanta", "Naranja", "$100", "https://ardiaprod.vtexassets.com/arquivos/ids/228481/Gaseosa-Fanta-naranja-15-Lts-_1.jpg?v=637959904131930000", 20)
+productos.addProduct("Cepita", "Sabor Naranja", "$150", "https://d2r9epyceweg5n.cloudfront.net/stores/001/312/763/products/jugo-cepita-naranja-litro-11-9cffefafb7e519f40115981375287957-1024-1024.jpg", 20 )
+
+console.log(productos)
+
+const copia = productos.getProductsById();
+
+console.log(copia)
